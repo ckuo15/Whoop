ae2c9e2ae327458b9d475e4a1f22d00c0c89fbd6
Suhaib <suhaib92khan@gmail.com>
2020-02-11 21:38:43 -0500

    Pulling master commit

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import { Switch } from 'react-router-dom';
@@ -6,7 +5,7 @@ import MainPage from "./main/main_page";
 import LoginFormContainer from './session/login_form_container';
 import SignupFormContainer from './session/signup_form_container';
 import NavBarContainer from "./nav/navbar_container";
-
+import SearchContainer from './search/search_container';
 
 const App = () => (
   <div>
@@ -14,6 +13,7 @@ const App = () => (
       <AuthRoute exact path="/" component={MainPage} />
       <AuthRoute exact path="/login" component={LoginFormContainer} />
       <AuthRoute exact path="/signup" component={SignupFormContainer} />
+      <AuthRoute exact path="/search" component={SearchContainer} />
     </Switch>
   </div>
 );
